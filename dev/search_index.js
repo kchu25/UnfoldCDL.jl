2d1641563d38920d7f1c63a5ebc730d6dd21ef86
Documenter.jl <documenter@juliadocs.github.io>
2022-11-05 07:04:04 +0000

    build based on 21ee809

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = uCDL","category":"page"},{"location":"#uCDL","page":"Home","title":"uCDL","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for uCDL.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [uCDL]","category":"page"}]
+}
